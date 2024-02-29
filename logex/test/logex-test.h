fdb3cfbd200eea1c379a66add596dfcfecfefbbf
Mark <mark.s.public@gmail.com>
2024-02-29 14:22:32 -0500

    Removed test directory

--- logex/test/logex-test.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOGEX_TEST_H__
-#define __LOGEX_TEST_H__
-
-#define TEST_DEFAULT(expect) \
-    printf("%s default value: %s (expecting %s)\n", __FILE__, LEVEL_STRING(LOG_LEVEL), LEVEL_STRING(expect))
-
-void other_file_default_test(int expect);
-
-#endif
